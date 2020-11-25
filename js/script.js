2c64da4b630a86ba056c0372feb94d0c8467827c
Leonardo Capogna <leonardocapogna@gmail.com>
2020-11-25 23:43:01 +0100

    js

--- js/script.js
+++ js/script.js
@@ -3,3 +3,28 @@
 // dropdown associate alle voci: "Abbonamenti", "Chi Siamo" e "Aiuto".
 // Consiglio: il focus dell'esercizio è la parte JS/jQuery,
 //  non è necessario il menu sia responsive.
+
+$(document).ready(function () {
+
+  $('#abbonamenti').mouseenter (function() {
+     $('#abbonamenti-dropdown').show(function() {
+     $('.activeDropdown').removeClass('activeDropdown').hide();
+     $('#abbonamenti-dropdown').addClass('activeDropdown');
+   });
+ });
+
+  $('#chi_siamo').mouseenter(function() {
+     $('#chi_siamo-dropdown').show(function() {
+     $('.activeDropdown').removeClass('activeDropdown').hide();
+     $('#chi_siamo-dropdown').addClass('activeDropdown');
+   });
+ });
+
+  $('#aiuto').mouseenter(function() {
+    $('#aiuto-dropdown').show(function() {
+    $('.activeDropdown').removeClass('activeDropdown').hide();
+    $('#aiuto-dropdown').addClass('activeDropdown');
+   });
+ });
+
+})
