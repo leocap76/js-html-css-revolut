83d8c19322e9310474790a84a602e9a65651b8c5
Leonardo Capogna <leonardocapogna@gmail.com>
2020-11-25 16:18:26 +0100

    css

---
+++ js/script.js
@@ -0,0 +1,5 @@
+// Gestire, prima al passaggio del mouse (come la versione online)
+//  e poi con il click, l'apertura delle 3
+// dropdown associate alle voci: "Abbonamenti", "Chi Siamo" e "Aiuto".
+// Consiglio: il focus dell'esercizio è la parte JS/jQuery,
+//  non è necessario il menu sia responsive.
