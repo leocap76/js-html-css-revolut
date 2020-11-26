64a4abf98522ccec42527577c419758b4b9d1406
Leonardo Capogna <leonardocapogna@gmail.com>
2020-11-26 15:31:55 +0100

    fix

--- js/script.js
+++ js/script.js
@@ -6,25 +6,36 @@
 
 $(document).ready(function () {
 
-  $('#abbonamenti').mouseenter (function() {
-     $('#abbonamenti-dropdown').show(function() {
-     $('.activeDropdown').removeClass('activeDropdown').hide();
-     $('#abbonamenti-dropdown').addClass('activeDropdown');
-   });
- });
+  $(".parent-dropdown").mouseenter(function() {
+    $(this).children(".activeDropdown").show();
+  });
 
-  $('#chi_siamo').mouseenter(function() {
-     $('#chi_siamo-dropdown').show(function() {
-     $('.activeDropdown').removeClass('activeDropdown').hide();
-     $('#chi_siamo-dropdown').addClass('activeDropdown');
-   });
- });
+  $(".parent-dropdown").mouseleave(function() {
+    $(this).children(".activeDropdown").hide();
+  });
+ //  $('#abbonamenti').mouseenter (function() {
+ //     $('#abbonamenti-dropdown').show(function() {
+ //     $('.activeDropdown').removeClass('activeDropdown').hide();
+ //     $('#abbonamenti-dropdown').addClass('activeDropdown');
+ //   });
+ // });
+ //
+ //  $('#chi_siamo').mouseenter(function() {
+ //     $('#chi_siamo-dropdown').show(function() {
+ //     $('.activeDropdown').removeClass('activeDropdown').hide();
+ //     $('#chi_siamo-dropdown').addClass('activeDropdown');
+ //   });
+ // });
+ //
+ //  $('#aiuto').mouseenter(function() {
+ //    $('#aiuto-dropdown').show(function() {
+ //    $('.activeDropdown').removeClass('activeDropdown').hide();
+ //    $('#aiuto-dropdown').addClass('activeDropdown');
+ //   });
+ // });
 
-  $('#aiuto').mouseenter(function() {
-    $('#aiuto-dropdown').show(function() {
-    $('.activeDropdown').removeClass('activeDropdown').hide();
-    $('#aiuto-dropdown').addClass('activeDropdown');
-   });
- });
-
-})
+});
+// $("#abbonamenti").click( function(){
+//   $(this).sibiling("#abbonamenti").children("#chi_siamo-dropdown").remove('activeDropdown');
+//   $(this).children("#chi_siamo-dropdown").toggleclass('activeDropdown');
+// })
